94dcb158f78797e1b3c95dbdcba04e75ed717fe1
jin0629 <jinw0o0k@naver.com>
2024-10-08 19:20:15 +0900

    Add files via upload

---
+++ hw5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void binaryNumber(int num)
+{
+    if(num==0)
+        return;
+
+    binaryNumber(num/2);
+
+    printf("%d",num%2);
+
+}
+
+int main(void)
+{
+    int num;
+    printf("2진수로 변환할 수를 입력하세요: ");
+    scanf("%d",&num);
+    printf("2진수로 변환한 값은:");
+    binaryNumber(num);
+    return 0;
+}
